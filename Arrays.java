c6845e8891794de9ae15a2d3cd9267c1ffd0a3ee
rsingh09 <38470058+rsingh09@users.noreply.github.com>
2020-09-11 04:15:45 -0400

    Add files via upload
    next 5

--- Arrays.java
+++ Arrays.java
@@ -36,11 +36,11 @@ public class Arrays {
 	}
 
 	public void rotate(int[] nums, int k) {
-        int len = nums.length;
-        k = k%len;
-        int[] res = new int[len];
+		int len = nums.length;
+		k = k % len;
+		int[] res = new int[len];
 		for (int i = 0; i < len; i++) {
-            res[(i+k)%len] = nums[i];
+			res[(i + k) % len] = nums[i];
 		}
 		System.arraycopy(res, 0, nums, 0, len);
 //		
@@ -71,16 +71,120 @@ public class Arrays {
 		}
 		return null;
 	}
-	
+
 	public int singleNumber(int[] nums) {
 		HashSet<Integer> val = new HashSet<Integer>();
-        for(int i=0;i<nums.length;i++)
-        {
-        	if(val.contains(nums[i]))
-        		val.remove(nums[i]);
-        	else
-        		val.add(nums[i]);
-        }
-        return val.iterator().next();
-    }
+		for (int i = 0; i < nums.length; i++) {
+			if (val.contains(nums[i]))
+				val.remove(nums[i]);
+			else
+				val.add(nums[i]);
+		}
+		return val.iterator().next();
+	}
+
+	public boolean containsDuplicate(int[] nums) {
+		HashSet<Integer> vals = new HashSet<Integer>();
+		for (int i : nums) {
+			if (vals.contains(i))
+				return true;
+			vals.add(i);
+		}
+		return false;
+	}
+
+	public int[] intersect(int[] nums1, int[] nums2) {
+		int[] result = new int[nums1.length];
+		int count = 0;
+		HashMap<Integer, Integer> vals = new HashMap<Integer, Integer>();
+		for (int i : nums1)
+			vals.put(i, vals.getOrDefault(i, 0) + 1);
+		for (int i : nums2) {
+			if (vals.containsKey(i)) {
+				result[count] = i;
+				count++;
+				int cnt = vals.get(i);
+				if (cnt == 0)
+					vals.put(i, cnt - 1);
+			}
+		}
+		return result;
+	}
+
+	public int[] plusOne(int[] digits) {
+		int len = digits.length - 1;
+		for (int i = len; i > -1; i--) {
+			if (digits[i] == 9)
+				digits[i] = 0;
+			else {
+				digits[i] += 1;
+				return digits;
+			}
+		}
+		digits = new int[len + 2];
+		digits[0] = 1;
+		return digits;
+	}
+
+	public void moveZeroes(int[] nums) {
+		int i = 0;
+		int j = 0;
+		while (j < nums.length) {
+			if (nums[j] != 0) {
+				nums[i++] = nums[j];
+			}
+
+			j++;
+		}
+		while (i < nums.length) {
+			nums[i++] = 0;
+		}
+	}
+
+	public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+		ListNode l3 = null;
+		ListNode temp = null;
+		int carry = 0;
+		while (l1 != null || l2 != null) {
+			int sum = 0;
+			int a = 0, b = 0;
+			if (l1 != null) {
+				a = l1.val;
+				// System.out.println("Val A: " + a);
+				l1 = l1.next;
+			}
+			if (l2 != null) {
+				b = l2.val;
+				// System.out.println("Val B: " + b);
+				l2 = l2.next;
+			}
+			sum = a + b + carry;
+			carry = sum > 9 ? 1 : 0;
+			sum = sum % 10;
+			if (temp == null)
+				temp = new ListNode(sum);
+			if (l3 == null) {
+				l3 = temp;
+			} else {
+				temp.next = new ListNode(sum);
+				temp = temp.next;
+				temp.next = null;
+			}
+		}
+		if (carry != 0) {
+			temp.next = new ListNode(carry);
+			temp = temp.next;
+			temp.next = null;
+		}
+		return l3;
+	}
+
+	public class ListNode {
+		int val;
+		ListNode next;
+
+		ListNode(int x) {
+			val = x;
+		}
+	}
 }
